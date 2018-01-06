0a3aec42f4c9047f739dc11b252b2a4a249bb12f
fabmu <fabmu@DESKTOP-864MMOH.Home>
2018-01-06 01:23:50 -0200

    Começando a calculadora de acordo com o livro
    JAVAFX INTERFACES DE QUALIDADE PARA APLICAÇÕES DESKTOP

--- Calculadora/src/application/Main.java
+++ Calculadora/src/application/Main.java
@@ -3,21 +3,33 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 
-public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+public class Main extends Application { 			// classe de visualização e controle extendida a aplication do JavaFx
+	
+	@Override             								// sobrescrever o método start
+	public void start (Stage stage) throws Exception {  	// método start (lógica e componentes)
+		
+		AnchorPane pane = new AnchorPane(); 					//criar um painel
+		pane.setPrefSize(400, 300);
+		
+		Scene scene = new Scene (pane);								// criar cena e passa o painel
+		
+		stage.setScene(scene); 											// indicar a cena utilizada no stage
+		
+		stage.show();														// abrir o stage
+		
+		TextField txtViewAritmetica = new TextField(); // mostar o cálculo feito, por ex: 10 - 15 = 
+		txtViewAritmetica.setPromptText("0");
+		
+		TextField txtViewResposta = new TextField();		// mostrar o resultado do cálculo aritmético
+		txtViewResposta.setPromptText("0");
+		
+		
+		
+		
 	}
 	
 	public static void main(String[] args) {
